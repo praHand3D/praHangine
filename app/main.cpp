cd55b765b0528c929f6a8169d2ed31d40819cad5
oliwiawaszczuk <oliwiawaszczuk12321@gmail.com>
2026-05-29 22:57:19 +0200

    simple window #3

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,33 @@
-#include <cstdio>
+#include <GLFW/glfw3.h>
+
+
+GLFWwindow* createWindow() {
+    if (!glfwInit())
+        return nullptr;
+
+    GLFWwindow *window = glfwCreateWindow(444, 444, "praHangine", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return nullptr;
+    }
+
+    return window;
+}
+
+void loop(GLFWwindow* window) {
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+}
 
 int main() {
-    printf("Hello, world from app!\n");
+    GLFWwindow* window = createWindow();
+    if (!window) return -1;
+
+    loop(window);
+
+    glfwTerminate();
     return 0;
 }

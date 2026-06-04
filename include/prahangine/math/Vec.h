3fe4b52a3afdae6fca212266cac65083af922ff7
oliwiawaszczuk <oliwiawaszczuk12321@gmail.com>
2026-06-04 14:36:40 +0200

    added constructors for types

--- include/prahangine/math/Vec.h
+++ include/prahangine/math/Vec.h
@@ -3,8 +3,12 @@
 
 struct Vec3 {
     float x, y, z;
+    Vec3(float x, float y, float z) : x(x), y(y), z(z) {};
+    Vec3(): x(0), y(0), z(0) {};
 };
 
 struct Vec4 {
     float x, y, z, w;
+    Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {};
+    Vec4(): x(0), y(0), z(0), w(0) {};
 };

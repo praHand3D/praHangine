bd6398023c1002138b15a5bbec27296fbffb044c
oliwiawaszczuk <oliwiawaszczuk12321@gmail.com>
2026-06-04 14:46:23 +0200

    created sandbox enviroment and one new function to create cube in .PR4

---
+++ src/utils/createCube.cpp
@@ -0,0 +1,50 @@
+#include "prahangine/utils.h"
+#include "prahangine/pr4/AssetSystem.h"
+#include "prahangine/pr4/types.h"
+#include "prahangine/config.h"
+
+void createCube() {
+    AssetSystem writer;
+
+    Mesh mesh;
+    mesh.id = 1;
+
+    mesh.vertexes.emplace_back(0,1,0);
+    mesh.vertexes.emplace_back(1,1,0);
+    mesh.vertexes.emplace_back(0,0,0);
+    mesh.vertexes.emplace_back(1,0,0);
+
+    mesh.vertexes.emplace_back(0,1,1);
+    mesh.vertexes.emplace_back(1,1,1);
+    mesh.vertexes.emplace_back(0,0,1);
+    mesh.vertexes.emplace_back(1,0,1);
+
+    mesh.indices.emplace_back(0,1,2);
+    mesh.indices.emplace_back(1,2,3);
+    mesh.indices.emplace_back(4,5,6);
+    mesh.indices.emplace_back(5,6,7);
+    mesh.indices.emplace_back(0,1,4);
+    mesh.indices.emplace_back(1,4,5);
+    mesh.indices.emplace_back(2,3,6);
+    mesh.indices.emplace_back(3,6,7);
+    mesh.indices.emplace_back(0,2,4);
+    mesh.indices.emplace_back(2,4,6);
+    mesh.indices.emplace_back(1,3,5);
+    mesh.indices.emplace_back(3,5,7);
+
+    writer.header.mesh_count = 1;
+    writer.meshes.push_back(mesh);
+
+    Transform transform = Transform(
+        Vec3(-0.5, -0.5, -0.5),
+        Vec4(0,0,0,0),
+        Vec3(1,1,1));
+
+    Object object = Object(1, 1, transform);
+    writer.header.object_count = 1;
+    writer.objects.push_back(object);
+
+    std::string path = DATA_PATH + "cube.PR4";
+
+    writer.write(path.c_str());
+}

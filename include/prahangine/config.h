17c52cf101224b0be856dd92cc0a1e94b89c1e38
oliwiawaszczuk <oliwiawaszczuk12321@gmail.com>
2026-06-04 14:40:32 +0200

    very important config - DATA_PATH

--- include/prahangine/config.h
+++ include/prahangine/config.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <string>
 
 constexpr uint8_t PR4_MAGIC[4] = { 0x50, 0x52, 0x34, 0x34 };
 constexpr uint8_t PR4_VERSION = 1;
+extern const std::string DATA_PATH;
